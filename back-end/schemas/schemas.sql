1a03ed6adce63e9f30890d7510fc7d8d29a7d9e0
your_github_username <rabi.abdelrahman@gmail.com>
2021-01-17 01:39:25 +0200

    create tables Post and Book_list

--- back-end/schemas/schemas.sql
+++ back-end/schemas/schemas.sql
@@ -1,25 +1,9 @@
-CREATE DATABASE IF NOT EXISTS BookClub;
+CREATE DATABASE IF NOT EXISTS book_club;
 
-USE BookClub;
-
------------------------------------
--- table Users
------------------------------------
-CREATE TABLE users (
-    user_id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    role_id INT NOT NULL,
-    username VARCHAR(50)  NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    phone VARCHAR(255)  NOT NULL,
-    created_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, username),
-    FOREIGN KEY (role_id) REFERENCES roles (role_id)
-);
+USE book_club;
 
 -----------------------------------
--- table Users
+-- table Roles
 -----------------------------------
 CREATE TABLE roles (
     role_id INT UNIQUE NOT NULL AUTO_INCREMENT,
@@ -27,3 +11,46 @@ CREATE TABLE roles (
     PRIMARY KEY (role_id)
 );
 
+-----------------------------------
+-- table Users
+-----------------------------------
+CREATE TABLE users (
+    user_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    role_id INT NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    phone INT (20)  NOT NULL,
+    country VARCHAR (50) NOT NULL,
+    created_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, username),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id)
+);
+
+-----------------------------------
+-- table Post
+-----------------------------------
+CREATE TABLE post
+(
+  post_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  likeCounter INT (20),
+  PRIMARY KEY (post_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-----------------------------------
+-- table Book List
+-----------------------------------
+CREATE TABLE book_list
+(
+  book_list_id INT UNIQUE NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  reading INT (25),
+  have_read INT (25),
+  to_read INT (25),
+  PRIMARY KEY (book_list_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
